09ba6a0a38b7e881d6a20e2c980c7195d8d26ead
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-05-11 12:10:59 +0700

    some refactor send payment

--- Query_typing_v1/query_typing.js
+++ Query_typing_v1/query_typing.js
@@ -3,18 +3,9 @@ var PaymentStatus;
     PaymentStatus["SUCCESS"] = "success";
     PaymentStatus["FAIL"] = "fail";
 })(PaymentStatus || (PaymentStatus = {}));
-function sendPayment(payment, endpoint) {
-    var socket = new WebSocket(endpoint);
-    socket.onopen = function () {
-        // socket.send('hello from the client');
-        // socket.json.send(payment);
-        socket.send(JSON.stringify(payment));
-    };
-    // socket.emit('onopen', payment);
-    socket.onmessage = function (response) {
-        var obj = JSON.parse(response.data);
-        console.log('JSON response from server: ', obj);
-        if (obj.status == 'success')
-            return obj;
-    };
+function getResultOfPayment(response) {
+    var obj = JSON.parse(response.data);
+    // console.log('JSON response from server: ', obj);
+    if (obj.status == 'success')
+        return obj;
 }
--- Query_typing_v1/query_typing.ts
+++ Query_typing_v1/query_typing.ts
@@ -31,23 +31,10 @@ interface IResponseFail {
 }
 
 type response = IResponseSuccess | IResponseFail;
-function sendPayment(payment: IPayment, endpoint: string): IResponseSuccess | IResponseFail {
 
-    let socket = new WebSocket(endpoint);
-
-    socket.onopen = function () {
-        // socket.send('hello from the client');
-        // socket.json.send(payment);
-        socket.send( JSON.stringify(payment) );
-    };
-
-    // socket.emit('onopen', payment);
-
-
-    socket.onmessage = function (response) {
-        let obj = JSON.parse(response.data);
-        console.log('JSON response from server: ', obj);
-        if (obj.status == 'success')
-            return obj;
-    };
+function getResultOfPayment(response): IResponseSuccess | IResponseFail {
+    let obj = JSON.parse(response.data);
+    // console.log('JSON response from server: ', obj);
+    if (obj.status == 'success')
+        return obj;
 }
