d124adaf7a7cf3fadddb91c9b15d8a8097f6190c
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-05-11 11:55:46 +0700

    send obj by socket

--- Query_typing_v1/query_typing.js
+++ Query_typing_v1/query_typing.js
@@ -6,8 +6,11 @@ var PaymentStatus;
 function sendPayment(payment, endpoint) {
     var socket = new WebSocket(endpoint);
     socket.onopen = function () {
-        socket.send('hello from the client');
+        // socket.send('hello from the client');
+        // socket.json.send(payment);
+        socket.send(JSON.stringify(payment));
     };
+    // socket.emit('onopen', payment);
     socket.onmessage = function (response) {
         var obj = JSON.parse(response.data);
         console.log('JSON response from server: ', obj);
