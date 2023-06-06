b5b66784b99698f7d41b4ba46a6ba54797c25786
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-06-06 14:23:03 +0700

    update groups

--- build/Generics/Group.js
+++ build/Generics/Group.js
@@ -8,6 +8,7 @@ let resultTemplate = {
     '1': [{ group: 1, name: 'a' }, { group: 1, name: 'b' }],
     '2': [{ group: 2, name: 'c' }],
 };
+//my realization
 const group = (arGroups, keyGroup) => {
     let result = {}, curGroupId;
     arGroups.map(groupObj => {
@@ -20,3 +21,20 @@ const group = (arGroups, keyGroup) => {
     return result;
 };
 console.log(group(dataInput, 'group'));
+// const group2<T> = (arGroups: T[], keyGroup: keyof T): IGroup => {
+function group2(arGroups, keyGroup) {
+    return arGroups.reduce((map, item) => {
+        const itemKey = item[keyGroup];
+        let curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        }
+        else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+const res_2 = group2(dataInput, 'group');
+console.log('res_2: ', res_2);
--- src/Generics/Group.ts
+++ src/Generics/Group.ts
@@ -13,7 +13,7 @@ let resultTemplate = {
     '1': [{group: 1, name: 'a'}, {group: 1, name: 'b'}],
     '2': [{group: 2, name: 'c'}],
 }
-
+//my realization
 const group = (arGroups: Data[], keyGroup: string): object => {
     let result: { [key: string]: Data[] } = {},
         curGroupId: string;
@@ -28,3 +28,28 @@ const group = (arGroups: Data[], keyGroup: string): object => {
 }
 
 console.log(group(dataInput, 'group'));
+
+//another realization
+interface IGroup {
+    [key: string]: T[];
+}
+
+type key = number|string|symbol;
+
+// const group2<T> = (arGroups: T[], keyGroup: keyof T): IGroup => {
+function group2<T extends Record<key, any>> (arGroups: T[], keyGroup: keyof T): IGroup {
+    return arGroups.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+        const itemKey = item[keyGroup];
+        let curEl = map[itemKey];
+        if(Array.isArray(curEl)){
+            curEl.push(item);
+        } else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+
+const res_2 = group2<Data>(dataInput, 'group');
+console.log('res_2: ', res_2);
